d4033427cea5487d1dcba4972a2634626450b894
Fildadubcek <dubcekf@gmail.com>
2021-03-23 11:43:04 +0100

    index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+//pohlaví
+$gender2 = "woman";
+$gender = "man";
+if ($gender == "male");
+elseif ($gender2 =="female");
+ {echo "If you choose $gender = You are a $gender."  ;}
+
+ {echo            "If you choose $gender2 = You are a $gender2 ";}
+ 
+ ?>
